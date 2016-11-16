a9a1341fbcf6e4ff6034403a3a23921e4f40ef4a
Yuan Gao <tinagao@google.com>
2016-11-16 09:48:53 -0800

    .

--- src/app/pages/components/components.ts
+++ src/app/pages/components/components.ts
@@ -1,18 +1,64 @@
-import { Component} from '@angular/core';
+import {Component} from '@angular/core';
+
+export class ComponentGroup {
+  name: string;
+  list: ComponentItem[];
+
+  constructor(name: string, list: ComponentItem[]) {
+    this.name = name;
+    this.list = list;
+  }
+}
+export class ComponentItem {
+  name: string;
+  src: string;
+  link: string;
+
+  constructor(name: string, src?: string, link?: string) {
+    this.name = name;
+    this.src = src ? src : name;
+    this.link = link ? link : this.src;
+  }
+
+  get routerLink() {
+    return '../component/' + this.link;
+  }
+
+  get imageSrc() {
+    return `../../../assets/img/components/${this.src}.svg`;
+  }
+}
 
 @Component({
   selector: 'app-components',
   templateUrl: 'components.html',
-  styleUrls: ['components.scss']
+  styleUrls: ['components.scss'],
 })
 export class ComponentsList {
-  componentItems = [
-    {name: 'Buttons', src: 'button'},
-    {name: 'Cards', src: 'card'},
-    {name: 'Chips', src: 'chip'},
-    {name: 'Grid lists', src: 'grid'},
-    {name: 'Menu', src: 'menu'},
-    {name: 'Tooltip', src: 'tooltip'},
-    {name: 'Progress', src: 'progress'},
+  componentGroups: ComponentGroup[] = [
+    new ComponentGroup('Form Controls', [
+      new ComponentItem('Chips', 'chip'),
+    ]),
+    new ComponentGroup('Layout Components', [
+      new ComponentItem('Cards', 'card'),
+      new ComponentItem('Grid lists', 'grid'),
+    ]),
+    new ComponentGroup('Pop-up and Notifications', [
+      new ComponentItem('Tooltip', 'tooltip'),
+      new ComponentItem('Menu', 'menu'),
+    ]),
+    new ComponentGroup('Miscellaneous', [
+      new ComponentItem('Progress', 'progress'),
+    ]),
   ]
+
+  gridMode: boolean = true;
+
+  get viewIcon() : string {
+    return this.gridMode ? 'lists' : 'apps';
+  }
+
+  switchViewMode() {
+    this.gridMode = !this.gridMode;
+  }
 }
