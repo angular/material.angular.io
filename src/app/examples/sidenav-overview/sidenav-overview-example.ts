2e79a6a27261f7dacf60ab0e858f770e63320113
Jeremy Elbourn <jelbourn@google.com>
2016-12-18 16:26:48 -0800

    Remove testing examples except for button

--- src/app/examples/sidenav-overview/sidenav-overview-example.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-
-@Component({
-  selector: 'sidenav-overview-example',
-  templateUrl: './sidenav-overview-example.html',
-})
-export class SidenavOverviewExample {}
--- src/app/examples/slider-overview/slider-overview-example.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-
-@Component({
-  selector: 'slider-overview-example',
-  templateUrl: './slider-overview-example.html',
-})
-export class SliderOverviewExample {}
