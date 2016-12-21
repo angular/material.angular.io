5fd0d43b7d6cf2f9e70e4bb5410a030c2740d70c
Jeremy Elbourn <jelbourn@google.com>
2016-12-21 15:26:10 -0800

    wip adding examples

--- src/app/examples/example-module.ts
+++ src/app/examples/example-module.ts
@@ -2,6 +2,54 @@ import {NgModule} from '@angular/core';
 import {MaterialModule} from '@angular/material';
 import {ButtonOverviewExample} from './button-overview/button-overview-example';
 import {ButtonTypesExample} from './button-types/button-types-example';
+import {CheckboxOverviewExample} from './checkbox-overview/checkbox-overview-example';
+import {SliderConfigurableExample} from './slider-configurable/slider-configurable-example';
+import {TabsOverviewExample} from './tabs-overview/tabs-overview-example';
+import {
+  SnackBarComponentExampleSnack,
+  SnackBarComponentExample
+} from './snack-bar-component/snack-bar-component-example';
+import {ProgressBarConfigurableExample} from './progress-bar-configurable/progress-bar-configurable-example';
+import {
+  DialogOverviewExampleDialog,
+  DialogOverviewExample
+} from './dialog-overview/dialog-overview-example';
+import {RadioNgModelExample} from './radio-ngmodel/radio-ngmodel-example';
+import {CardFancyExample} from './card-fancy/card-fancy-example';
+import {ToolbarOverviewExample} from './toolbar-overview/toolbar-overview-example';
+import {ToolbarMultirowExample} from './toolbar-multirow/toolbar-multirow-example';
+import {MenuIconsExample} from './menu-icons/menu-icons-example';
+import {GridListDynamicExample} from './grid-list-dynamic/grid-list-dynamic-example';
+import {IconOverviewExample} from './icon-overview/icon-overview-example';
+import {ProgressBarOverviewExample} from './progress-bar-overview/progress-bar-overview-example';
+import {SlideToggleOverviewExample} from './slide-toggle-overview/slide-toggle-overview-example';
+import {InputOverviewExample} from './input-overview/input-overview-example';
+import {MenuOverviewExample} from './menu-overview/menu-overview-example';
+import {CheckboxConfigurableExample} from './checkbox-configurable/checkbox-configurable-example';
+import {ButtonToggleExclusiveExample} from './button-toggle-exclusive/button-toggle-exclusive-example';
+import {ListSectionsExample} from './list-sections/list-sections-example';
+import {SnackBarOverviewExample} from './snack-bar-overview/snack-bar-overview-example';
+import {
+  DialogResultExampleDialog,
+  DialogResultExample
+} from './dialog-result/dialog-result-example';
+import {TooltipOverviewExample} from './tooltip-overview/tooltip-overview-example';
+import {ButtonToggleOverviewExample} from './button-toggle-overview/button-toggle-overview-example';
+import {GridListOverviewExample} from './grid-list-overview/grid-list-overview-example';
+import {ExampleData} from './example-data';
+import {TooltipPositionExample} from './tooltip-position/tooltip-position-example';
+import {ProgressCircleConfigurableExample} from './progress-circle-configurable/progress-circle-configurable-example';
+import {InputFormExample} from './input-form/input-form-example';
+import {ListOverviewExample} from './list-overview/list-overview-example';
+import {SliderOverviewExample} from './slider-overview/slider-overview-example';
+import {SlideToggleConfigurableExample} from './slide-toggle-configurable/slide-toggle-configurable-example';
+import {IconSvgExample} from './icon-svg-example/icon-svg-example';
+import {SidenavFabExample} from './sidenav-fab/sidenav-fab-example';
+import {CardOverviewExample} from './card-overview/card-overview-example';
+import {ProgressCircleOverviewExample} from './progress-circle-overview/progress-circle-overview-example';
+import {TabsTemplateLabelExample} from './tabs-template-label/tabs-template-label-example';
+import {RadioOverviewExample} from './radio-overview/radio-overview-example';
+import {SidenavOverviewExample} from './sidenav-overview/sidenav-overview-example';
 
 
 /**
@@ -21,7 +69,49 @@ export const EXAMPLE_COMPONENTS = {
  */
 export const EXAMPLE_LIST = [
   ButtonOverviewExample,
+  ButtonToggleExclusiveExample,
+  ButtonToggleOverviewExample,
   ButtonTypesExample,
+  CardFancyExample,
+  CardOverviewExample,
+  CheckboxConfigurableExample,
+  CheckboxOverviewExample,
+  DialogOverviewExample,
+  DialogOverviewExampleDialog,
+  DialogResultExample,
+  DialogResultExampleDialog,
+  ExampleData,
+  GridListDynamicExample,
+  GridListOverviewExample,
+  IconOverviewExample,
+  IconSvgExample,
+  InputFormExample,
+  InputOverviewExample,
+  ListOverviewExample,
+  ListSectionsExample,
+  MenuIconsExample,
+  MenuOverviewExample,
+  ProgressBarConfigurableExample,
+  ProgressBarOverviewExample,
+  ProgressCircleConfigurableExample,
+  ProgressCircleOverviewExample,
+  RadioNgModelExample,
+  RadioOverviewExample,
+  SidenavFabExample,
+  SidenavOverviewExample,
+  SliderConfigurableExample,
+  SliderOverviewExample,
+  SlideToggleConfigurableExample,
+  SlideToggleOverviewExample,
+  SnackBarComponentExample,
+  SnackBarComponentExampleSnack,
+  SnackBarOverviewExample,
+  TabsOverviewExample,
+  TabsTemplateLabelExample,
+  ToolbarMultirowExample,
+  ToolbarOverviewExample,
+  TooltipOverviewExample,
+  TooltipPositionExample,
 ];
 
 @NgModule({
