73e2dab85f4faa3b874bde257ea9f4e6327c7aca
crisbeto <crisbeto@abv.bg>
2016-12-22 14:43:14 +0200

    fix: errors in plunkr template
    * Fixes errors in the Plunkr template due to outdated Material version.
    * Updates the outdated theme link.
    * Disables the opening of the Plunk in a new tab, because it gets blocked by the browser as a popup.
    
    This is based on the Plunkr from our issue template.

--- src/assets/plunker/systemjs.config.js
+++ src/assets/plunker/systemjs.config.js
@@ -7,18 +7,6 @@ System.config({
   packages: {
     '.': {
       defaultExtension: 'ts'
-    }
-  }
-});
-/** Add Transpiler for Typescript */
-System.config({
-  transpiler: 'typescript',
-  typescriptOptions: {
-    emitDecoratorMetadata: true
-  },
-  packages: {
-    '.': {
-      defaultExtension: 'ts'
     },
     'vendor': {
       defaultExtension: 'js'
@@ -28,24 +16,24 @@ System.config({
 
 System.config({
   map: {
-    'rxjs': 'vendor/rxjs',
     'main': 'main.js',
+
     // Angular specific mappings.
-    '@angular/core': 'vendor/@angular/core/bundles/core.umd.js',
-    '@angular/common': 'vendor/@angular/common/bundles/common.umd.js',
-    '@angular/compiler': 'vendor/@angular/compiler/bundles/compiler.umd.js',
-    '@angular/http': 'vendor/@angular/http/bundles/http.umd.js',
-    '@angular/forms': 'vendor/@angular/forms/bundles/forms.umd.js',
-    '@angular/router': 'vendor/@angular/router/bundles/router.umd.js',
-    '@angular/platform-browser': 'vendor/@angular/platform-browser/bundles/platform-browser.umd.js',
-    '@angular/platform-browser-dynamic': 'vendor/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+    '@angular/core': 'http://unpkg.com/@angular/core/bundles/core.umd.js',
+    '@angular/common': 'http://unpkg.com/@angular/common/bundles/common.umd.js',
+    '@angular/compiler': 'http://unpkg.com/@angular/compiler/bundles/compiler.umd.js',
+    '@angular/http': 'http://unpkg.com/@angular/http/bundles/http.umd.js',
+    '@angular/forms': 'http://unpkg.com/@angular/forms/bundles/forms.umd.js',
+    '@angular/router': 'http://unpkg.com/@angular/router/bundles/router.umd.js',
+    '@angular/platform-browser': 'http://unpkg.com/@angular/platform-browser/bundles/platform-browser.umd.js',
+    '@angular/platform-browser-dynamic': 'http://unpkg.com/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+    '@angular/material': 'https://cdn.rawgit.com/angular/material2-builds/master/bundles/material.umd.js',
+
+    // Rxjs mapping
+    'rxjs': 'https://unpkg.com/rxjs',
   },
   packages: {
     // Thirdparty barrels.
     'rxjs': { main: 'index' },
-    '@angular/material': {
-      format: 'cjs',
-      main: 'material.umd.js'
-    }
   }
 });
