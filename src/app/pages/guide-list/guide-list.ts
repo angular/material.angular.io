46592d2f04c1837f8dd7d58ca8ae97ff7f6c44fe
Austin McDaniel <amcdaniel2@gmail.com>
2017-07-12 08:10:34 -0500

    feat(docs): add page titles to doc pages #189

--- src/app/pages/guide-list/guide-list.ts
+++ src/app/pages/guide-list/guide-list.ts
@@ -1,17 +1,22 @@
-import {Component, NgModule} from '@angular/core';
+import {Component, NgModule, OnInit} from '@angular/core';
 import {GuideItems} from '../../shared/guide-items/guide-items';
 import {MdListModule} from '@angular/material';
 import {RouterModule} from '@angular/router';
 import {FooterModule} from '../../shared/footer/footer';
 import {CommonModule} from '@angular/common';
+import {ComponentPageTitle} from '../page-title/page-title';
 
 @Component({
   selector: 'app-guides',
   templateUrl: './guide-list.html',
   styleUrls: ['./guide-list.scss']
 })
-export class GuideList {
-  constructor(public guideItems: GuideItems) {}
+export class GuideList implements OnInit {
+  constructor(public guideItems: GuideItems, public _componentPageTitle: ComponentPageTitle) {}
+
+  ngOnInit(): void {
+    this._componentPageTitle.title = 'Guides';
+  }
 }
 
 
--- src/app/pages/guide-viewer/guide-viewer.ts
+++ src/app/pages/guide-viewer/guide-viewer.ts
@@ -1,22 +1,28 @@
-import {Component, NgModule} from '@angular/core';
+import {Component, NgModule, OnInit} from '@angular/core';
 import {ActivatedRoute, RouterModule} from '@angular/router';
 import {GuideItem, GuideItems} from '../../shared/guide-items/guide-items';
 import {FooterModule} from '../../shared/footer/footer';
 import {DocViewerModule} from '../../shared/doc-viewer/doc-viewer-module';
-
+import {ComponentPageTitle} from '../page-title/page-title';
 
 @Component({
   selector: 'guide-viewer',
   templateUrl: './guide-viewer.html',
   styleUrls: ['./guide-viewer.scss'],
 })
-export class GuideViewer {
+export class GuideViewer implements OnInit {
   guide: GuideItem;
 
-  constructor(private _route: ActivatedRoute, public guideItems: GuideItems) {
+  constructor(private _route: ActivatedRoute,
+              private _componentPageTitle: ComponentPageTitle,
+              public guideItems: GuideItems) {
     _route.params.subscribe(p => {
       this.guide = guideItems.getItemById(p['id']);
     });
+  }
+
+  ngOnInit(): void {
+    this._componentPageTitle.title = this.guide.name;
   }
 }
 
