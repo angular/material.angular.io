8a9f7c1b7f4e83843812fbd74d9ebb2b66aa80a3
Will Howell <will.s.howell@gmail.com>
2017-08-08 11:31:42 -0400

    fix(doc-viewer): cancel previous pending requests when changing url
    -This fixes an issue where, on slow connections, requests would unnecessarily stack up if the user navigates components & tabs faster than requests can complete

--- src/app/shared/doc-viewer/doc-viewer.ts
+++ src/app/shared/doc-viewer/doc-viewer.ts
@@ -10,6 +10,7 @@ import {
 } from '@angular/core';
 import {Http} from '@angular/http';
 import {ComponentPortal, DomPortalHost} from '@angular/material';
+import {Subscription} from 'rxjs/Subscription';
 import {ExampleViewer} from '../example-viewer/example-viewer';
 
 
@@ -19,6 +20,7 @@ import {ExampleViewer} from '../example-viewer/example-viewer';
 })
 export class DocViewer implements OnDestroy {
   private _portalHosts: DomPortalHost[] = [];
+  private _documentFetchSubscription: Subscription;
 
   /** The URL of the document to display. */
   @Input()
@@ -35,7 +37,12 @@ export class DocViewer implements OnDestroy {
 
   /** Fetch a document by URL. */
   private _fetchDocument(url: string) {
-    this._http.get(url).subscribe(
+    // Cancel previous pending request
+    if (this._documentFetchSubscription) {
+      this._documentFetchSubscription.unsubscribe();
+    }
+
+    this._documentFetchSubscription = this._http.get(url).subscribe(
         response => {
           // TODO(mmalerba): Trust HTML.
           if (response.ok) {
@@ -88,5 +95,9 @@ export class DocViewer implements OnDestroy {
 
   ngOnDestroy() {
     this._clearLiveExamples();
+
+    if (this._documentFetchSubscription) {
+      this._documentFetchSubscription.unsubscribe();
+    }
   }
 }
