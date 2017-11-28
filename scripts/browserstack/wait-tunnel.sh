a64e5beb3f26a14f240a29f73b51b97fabd3592d
Jeremy Elbourn <jelbourn@google.com>
2017-11-28 11:12:37 -0800

    copy saucelabs setup from material2 repo

--- scripts/browserstack/wait-tunnel.sh
+++ scripts/browserstack/wait-tunnel.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 TUNNEL_LOG="$LOGS_DIR/browserstack-tunnel.log"
+WAIT_DELAY=30
+
+# Method that prints the logfile output of the browserstack tunnel.
+printLog() {
+  echo "Logfile output of Browserstack tunnel (${TUNNEL_LOG}):"
+  echo ""
+  cat ${TUNNEL_LOG}
+}
 
 # Wait for Connect to be ready before exiting
 # Time out if we wait for more than 2 minutes, so the process won't run forever.
@@ -10,19 +18,25 @@ let "counter=0"
 if [ -f $BROWSER_PROVIDER_ERROR_FILE ]; then
   echo
   echo "An error occurred while starting the tunnel. See error:"
-  cat $TUNNEL_LOG
+  printLog
   exit 5
 fi
 
 while [ ! -f $BROWSER_PROVIDER_READY_FILE ]; do
   let "counter++"
 
-  if [ $counter -gt 240 ]; then
+  # Counter needs to be multiplied by two because the while loop only sleeps a half second.
+  # This has been made in favor of better progress logging (printing dots every half second)
+  if [ $counter -gt $[${WAIT_DELAY} * 2] ]; then
     echo
     echo "Timed out after 2 minutes waiting for tunnel ready file"
+    printLog
     exit 5
   fi
 
   printf "."
-  sleep .5
+  sleep 0.5
 done
+
+echo ""
+echo "Connected to Browserstack"
