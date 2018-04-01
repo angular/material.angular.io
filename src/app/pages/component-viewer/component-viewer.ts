a4f1ddfdd47172daaf870d67576c64b7d719fdc3
Austin <amcdaniel2@gmail.com>
2018-04-01 12:06:38 -0500

    chore: address feedback

--- src/app/pages/component-viewer/component-viewer.ts
+++ src/app/pages/component-viewer/component-viewer.ts
@@ -16,9 +16,11 @@ import {TableOfContentsModule} from '../../shared/table-of-contents/table-of-con
 import {ComponentPageTitle} from '../page-title/page-title';
 import {combineLatest} from 'rxjs/observable/combineLatest';
 import {map} from 'rxjs/operators/map';
+import {takeUntil} from 'rxjs/operators/takeUntil';
 import {BreakpointObserver} from '@angular/cdk/layout';
 import {Observable} from 'rxjs/Observable';
 import {Subscription} from 'rxjs/Subscription';
+import {Subject} from 'rxjs/Subject';
 import {TableOfContents} from '../../shared/table-of-contents/table-of-contents';
 
 @Component({
@@ -31,6 +33,7 @@ export class ComponentViewer implements OnDestroy {
   componentDocItem: DocItem;
   sections: Set<string> = new Set(['overview', 'api']);
   private _subscription: Subscription;
+  private _destroyed = new Subject();
 
   constructor(private _route: ActivatedRoute,
               private router: Router,
@@ -39,9 +42,10 @@ export class ComponentViewer implements OnDestroy {
               ) {
     // Listen to changes on the current route for the doc id (e.g. button/checkbox) and the
     // parent route for the section (material/cdk).
-    this._subscription = combineLatest(_route.params, _route.parent.params).pipe(
+    combineLatest(_route.params, _route.parent.params).pipe(
         map((p: [Params, Params]) => ({id: p[0]['id'], section: p[1]['section']})),
-        map(p => ({doc: docItems.getItemById(p.id, p.section), section: p.section}))
+        map(p => ({doc: docItems.getItemById(p.id, p.section), section: p.section}),
+        takeUntil(this._destroyed))
         ).subscribe(d => {
           this.componentDocItem = d.doc;
           if (this.componentDocItem) {
@@ -54,6 +58,10 @@ export class ComponentViewer implements OnDestroy {
           }
         });
   }
+
+  ngOnDestroy(): void {
+    this._destroyed.next();
+  }
 }
 
 @Component({
@@ -64,10 +72,10 @@ export class ComponentViewer implements OnDestroy {
 export class ComponentOverview implements OnInit {
   @ViewChild('intialFocusTarget') focusTarget: ElementRef;
   @ViewChild('toc') tableOfContents: TableOfContents;
-  showToc$: Observable<boolean>;
+  showToc: Observable<boolean>;
 
   constructor(public componentViewer: ComponentViewer, breakpointObserver: BreakpointObserver) {
-    this.showToc$ = breakpointObserver.observe('(max-width: 1200px)')
+    this.showToc = breakpointObserver.observe('(max-width: 1200px)')
       .pipe(map(result => !result.matches));
   }
 
