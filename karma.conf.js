dfd8799c09493b00771971d46268a4caf5ccc6e9
Jeremy Elbourn <jelbourn@google.com>
2018-04-12 14:14:53 -0700

    Get unit tests running

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 const {customLaunchers, platformMap} = require('./browser-providers');
-const path = require('path');
 
 module.exports = function (config) {
   config.set({
@@ -10,34 +9,21 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-remap-istanbul'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-browserstack-launcher'),
       require('karma-sauce-launcher'),
     ],
-    files: [
-      {pattern: './src/test.ts', watched: false},
-      {pattern: 'node_modules/hammerjs/hammer.js', included: true, watched: false},
-    ],
-    preprocessors: {
-      './src/test.ts': ['@angular-devkit/build-angular']
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    mime: {
-      'text/x-typescript': ['ts','tsx']
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, 'coverage'),
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
     },
-    remapIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: {
-        html: 'coverage',
-        lcovonly: './coverage/coverage.lcov'
-      }
-    },
-    angularCli: {
-      config: './angular-cli.json',
-      environment: 'dev'
-    },
-    reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['dots', 'karma-remap-istanbul']
-              : ['dots'],
+    rreporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
