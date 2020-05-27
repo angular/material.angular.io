65bbd050c668d5664fea6ebb9d9aa1c715b60ab3
Annie Wang <annieyw@google.com>
2020-05-27 15:45:47 -0700

    new cli app

---
+++ scenes/src/app/input/input-scene.ts
@@ -0,0 +1,12 @@
+import {Component, NgModule} from '@angular/core';
+
+@Component({
+  selector: 'input-scene',
+  templateUrl: './input-scene.html',
+  styleUrls: ['./input-scene.css'],
+})
+export class InputScene {
+  value = 'Clear me';
+}
+
+@NgModule()
