b9aa834447375d65041815bde812fc375c043001
Annie Wang <annieyw@google.com>
2020-06-02 10:49:53 -0700

    drop scenes, retain just the set up

--- scenes/e2e/src/app.e2e-spec.ts
+++ scenes/e2e/src/app.e2e-spec.ts
@@ -1,20 +1,11 @@
 import {AppPage} from './app.po';
-import {screenshot} from '../screenshot';
 
 describe('screenshot scenes for each component', () => {
   // These tests simply serve as a convenient way to take snapshots of different pages,
   // they are not actually testing anything
   let page: AppPage;
-  const components = ['button', 'checkbox', 'datepicker', 'input'];
 
   beforeEach(() => {
     page = new AppPage();
   });
-
-  for (const comp of components) {
-    it('screenshot for ' + comp + ' scene', async () => {
-      await page.navigateTo(comp);
-      await screenshot(comp);
-    });
-  }
 });
--- scenes/src/app/app-routing.module.ts
+++ scenes/src/app/app-routing.module.ts
@@ -1,18 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {InputScene} from './scenes/input/input-scene';
-import {SceneViewer} from './scene-viewer/scene-viewer';
-import {ButtonScene} from './scenes/button/button-scene';
-import {CheckboxScene} from './scenes/checkbox/checkbox-scene';
-import {DatepickerScene} from './scenes/datepicker/datepicker-scene';
 
 
-const routes: Routes = [
-  {path: 'button', component: SceneViewer, data: {hueRotate: 0, scene: ButtonScene}},
-  {path: 'checkbox', component: SceneViewer, data: {hueRotate: 15, scene: CheckboxScene}},
-  {path: 'datepicker', component: SceneViewer, data: {hueRotate: 30, scene: DatepickerScene}},
-  {path: 'input', component: SceneViewer, data: {hueRotate: 45, scene: InputScene}},
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
