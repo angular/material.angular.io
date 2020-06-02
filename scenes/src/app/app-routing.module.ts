5ab2654e0bee88214aece6c62a60a4634ea7a790
Annie Wang <annieyw@google.com>
2020-06-02 16:41:31 -0700

    input scene

--- scenes/src/app/app-routing.module.ts
+++ scenes/src/app/app-routing.module.ts
@@ -4,9 +4,13 @@ import { SceneViewer } from './scene-viewer/scene-viewer';
 import { RipplesScene } from './scenes/ripples/ripples-scene';
 
 
+import { InputScene } from './scenes/input/input-scene';
+
+
 const routes: Routes = [
   {path: 'ripples', component: SceneViewer, data: {hueRotate: 120, scene: RipplesScene}},
   {path: 'slider', component: SceneViewer, data: {hueRotate: 135, scene: RipplesScene}},
+  {path: 'input', component: SceneViewer, data: {hueRotate: 45, scene: InputScene}},
 ];
 
 @NgModule({
