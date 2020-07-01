852b4c03a536c591c168fa414cd6ad10fb6aa5d9
Miles Malerba <mmalerba@google.com>
2020-07-01 15:03:27 -0700

    add grid list scene

--- scenes/src/app/app.module.ts
+++ scenes/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {DOCUMENT} from '@angular/common';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {BottomSheetSceneModule} from './scenes/bottom-sheet/bottom-sheet-scene';
+import {GridListSceneModule} from './scenes/grid-list/grid-list-scene';
 import {SnackBarSceneModule} from './scenes/snack-bar/snack-bar-scene';
 
 @NgModule({
@@ -31,6 +32,7 @@ import {SnackBarSceneModule} from './scenes/snack-bar/snack-bar-scene';
     MatMenuModule,
     MatPaginatorModule,
     BottomSheetSceneModule,
+    GridListSceneModule,
     SnackBarSceneModule,
   ],
   bootstrap: [AppComponent],
