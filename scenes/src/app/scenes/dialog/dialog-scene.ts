a951ce93c37bc3d00026adeeb2ba5bec5efad477
Annie Wang <annieyw@google.com>
2020-07-07 14:58:33 -0700

    reduce dialog size, use icon for tooltip

--- scenes/src/app/scenes/dialog/dialog-scene.ts
+++ scenes/src/app/scenes/dialog/dialog-scene.ts
@@ -14,8 +14,8 @@ export class DialogScene {
 
   openDialog() {
     this.dialog.open(DialogSceneExampleDialog, {
-      height: '190px',
-      width: '300px',
+      // height: '80%',
+      // width: '80%',
       hasBackdrop: false
     });
   }
@@ -34,12 +34,11 @@ export class DialogSceneModule {
 @Component({
   selector: 'app-dialog-scene-example-dialog',
   template: `
-    <h1 mat-dialog-title>Reset settings</h1>
-    <div mat-dialog-content>This will reset your device to its default factory settings.</div>
+    <div mat-dialog-content>Discard draft?</div>
     <div mat-dialog-actions>
       <button mat-button mat-dialog-close>Cancel</button>
-      <button mat-button mat-dialog-close>Accept</button>
-    </div>`
+      <button mat-button mat-dialog-close>Discard</button>
+    </div>`,
 })
 export class DialogSceneExampleDialog {}
 
