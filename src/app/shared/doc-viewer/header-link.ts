ef7ea74c6aaba7914df4608e0a73ca82f8cd6688
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-09-24 10:40:02 +0000

    fixup! refactor: migrate to signal inputs using automated migration
    Example is not an input due to some lazy instantiation in doc viewer

--- src/app/shared/doc-viewer/header-link.ts
+++ src/app/shared/doc-viewer/header-link.ts
@@ -1,4 +1,4 @@
-import {Component, input} from '@angular/core';
+import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 
